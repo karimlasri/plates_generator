e69e814cd712e33dda41559063ad246263e7dda6
Karim Lasri <karim.lasri@student.ecp.fr>
2018-04-15 03:00:11 +0200

    Changed dimensions of plates

--- plate_generator.cpp
+++ plate_generator.cpp
@@ -31,50 +31,67 @@ int main(int argc, char **argv)
     nb = Split(vect, src_name, '.');
     src_name = vect[0];
 
-
     Mat original = imread(imgpath, IMREAD_COLOR);
     Mat img = imread(imgpath, IMREAD_COLOR);
     Point2f corners[4];
-    cout << to_string(img.size().width) << " " << to_string(img.size().height) << "\n";
-    // char original_label[PLATE_NUMBER_SIZE];
-    srand(time(NULL));
-    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
-    std::default_random_engine generator(seed);
-    std::normal_distribution<double> distribution(0.0, 10.0);
-    //int pan_angle = rand() % 90 - 45;
-    int pan_angle = int(distribution(generator));
-    float angle_ratio = 1.5;
-    int orig_size_ratio = 5;
-    int orig_width = (img.size().width) / 10 + (rand() % (img.size().width) / 15);
-    int orig_height = orig_width / orig_size_ratio;
-    int target_width = orig_width * (1 - sin(2 * M_PI * pan_angle / angle_ratio / 360));
-    int offset = orig_height * sin(2 * M_PI * pan_angle / 360);
-    cv::Point2f bl, tr, br;
-    if (pan_angle > 0)
-    {
-        tr = cv::Point2f(target_width, offset);
-        br = cv::Point2f(target_width, orig_height * (1 + sin(2 * M_PI * pan_angle / angle_ratio / 360)) + offset);
-        bl = cv::Point2f(0, orig_height * (1 - sin(2 * M_PI * pan_angle / angle_ratio / 360)));
-    }
-    else if (pan_angle < 0)
-    {
-        tr = cv::Point2f(target_width, -offset);
-        br = cv::Point2f(target_width, orig_height * (1 - sin(2 * M_PI * pan_angle / angle_ratio / 360)) - offset);
-        bl = cv::Point2f(0, orig_height * (1 + sin(2 * M_PI * pan_angle / angle_ratio / 360)));
-    }
 
-    cv::Point2f tl = cv::Point2f(rand() % (img.size().width - target_width), offset + (rand() % int(img.size().height - orig_height * (1 + sin(pan_angle / angle_ratio) - 2 * offset))));
+    unsigned long seed = mix(clock(), time(NULL), getpid());
+    std::cout << "____" << "\n";
+//    std::cout << "CLOCK variables : " << to_string(clock()) << " " << to_string(time(NULL)) << " " << to_string(getpid()) << " " << seed << "\n" ;
+    srand(seed);
+    std::default_random_engine generator(seed);
+    std::normal_distribution<double> distribution(0, 20);
+    // Angle for pan
+    int pan_angle = min(25, max(-25, int(distribution(generator))));
+//    std::cout << "pan angle : " << to_string(pan_angle) << "\n";
+    // Angle ratio used for the shift between right and left sides
+    int angle_ratio = 1.5;
+    // Ratio between width and height in original image
+    int orig_size_ratio = 5;
+    // Original centered plate dimensions
+    int orig_width = (img.size().width) / 4 + (rand() % (img.size().width) / 12);
+    int orig_height = orig_width / orig_size_ratio;
+    // Transformed plate dimensions
+    int target_width = orig_width * (1 - sin(2 * M_PI *abs(pan_angle) / angle_ratio / 360));
+//    std::cout << "Target width : " << to_string(target_width) << "\n";
+    // Shift between right and left angles
+    int offset = orig_height * sin(2 * M_PI * pan_angle / 360);
+//    std:cout << "offset : " << to_string(offset) << "\n";
+    // Coordinates of the corners of transformed plate relative to top-left angle
+    cv::Point2f bl, tr, br;
+//    if (pan_angle > 0)
+   // {
+    tr = cv::Point2f(target_width, offset);
+    br = cv::Point2f(target_width, orig_height * (1 + sin(2 * M_PI * pan_angle / angle_ratio / 360)) + offset);
+    bl = cv::Point2f(0, orig_height * (1 - sin(2 * M_PI * pan_angle / angle_ratio / 360)));
+    //}
+   // else
+   // {
+       // tr = cv::Point2f(target_width, -offset);
+     //   br = cv::Point2f(target_width, orig_height * (1 - sin(2 * M_PI * pan_angle / angle_ratio / 360)) - offset);
+    //    bl = cv::Point2f(0, orig_height * (1 + sin(2 * M_PI * pan_angle / angle_ratio / 360)));
+    //}
+//    std::cout << "img height : " << to_string(img.size().height) << "\n";
+//    std::cout << "orig height : " << to_string(orig_height) << "\n";
+//    std::cout << "rad angle : " << to_string(2*M_PI*abs(pan_angle)/angle_ratio/360) << "\n" ;
+//    std::cout << "tl modulo : " << to_string(int(img.size().height - orig_height*(1+sin(2*M_PI*abs(pan_angle)/angle_ratio/360))-2*offset)-1)<< "\n";
+    cv::Point2f tl = cv::Point2f(rand() % (img.size().width - target_width-1), abs(offset) + orig_height*sin(2*M_PI*abs(pan_angle)/angle_ratio/360)+(rand() % int(img.size().height - orig_height * (1 + sin(2*M_PI*abs(pan_angle)/ angle_ratio / 360)) - 2 *abs(offset))-1));
+//    std::cout << "Image dimensions : (w, h)" << img.size().width << " " << img.size().height << "\n";
+    // Coordinates of the transformed plate's corners in target image
     corners[0] = tl;
     corners[1] = cv::Point2f(tl.x + tr.x, tl.y + tr.y);
     corners[2] = cv::Point2f(tl.x + br.x, tl.y + br.y);
     corners[3] = cv::Point2f(tl.x + bl.x, tl.y + bl.y);
+//    std::cout << "Corners of transf (x, y): " << to_string(corners[0].x) << " " << to_string(corners[0].y) << " " << to_string(corners[1].x) << " " << to_string(corners[1].y) << " " << to_string(corners[2].x) << " " << to_string(corners[2].y) << to_string(corners[3].x) << " " << to_string(corners[3].y) << "\n";
 
+    // Contour of transformed plate with integer coordinates
     cv::Point2i contours[4];
     contours[0] = cv::Point2i(corners[0]);
     contours[1] = cv::Point2i(corners[1]);
     contours[2] = cv::Point2i(corners[2]);
     contours[3] = cv::Point2i(corners[3]);
 
+    // Generating plate and putting it on target image
     char number[PLATE_NUMBER_SIZE];
     generate_random_number(number);
     put_generated_number(img, contours, number);
